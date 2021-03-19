81976cbada34e7fb7b995a9ac25a8034b41dc986
eaglesv2 <74346502+eaglesv2@users.noreply.github.com>
2021-03-19 17:47:44 +0900

    메뉴 설정

--- HiWorld/src/main/java/com/hiworld/minihp/service/MiniHpSettingServiceImpl.java
+++ HiWorld/src/main/java/com/hiworld/minihp/service/MiniHpSettingServiceImpl.java
@@ -4,7 +4,6 @@ package com.hiworld.minihp.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.hiworld.client.vo.ClientVO;
 import com.hiworld.minihp.dao.MiniHpSettingDAO;
 import com.hiworld.minihp.vo.MiniHpUserMenuVO;
 
