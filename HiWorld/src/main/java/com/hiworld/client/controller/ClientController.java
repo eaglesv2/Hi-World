df996014c6ba09834e227b20e5a0c9f3ff2561f9
stux12 <stux12@naver.com>
2021-04-05 17:33:44 +0900

    검색기능구현완료

--- HiWorld/src/main/java/com/hiworld/client/controller/ClientController.java
+++ HiWorld/src/main/java/com/hiworld/client/controller/ClientController.java
@@ -6,7 +6,7 @@ import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
+import java.util.Locale;
 
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
@@ -21,7 +21,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -29,6 +28,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.google.gson.Gson;
 import com.hiworld.article.service.ArticleService;
 import com.hiworld.article.vo.ArticleVO;
 import com.hiworld.article.vo.MainShoppingPagingVO;
@@ -236,20 +236,19 @@ public class ClientController {
 	}
 
 //  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 이웃 찾기 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-	/* 이웃 찾기 */
-	@GetMapping("/boardPage.do")
-	public String boardAjax(Model model, HttpSession session, ServletResponse res) throws Exception {
-
-		/* 내 아이디 가져오기 */
-		sessionVO vo = (sessionVO) session.getAttribute("sessionVO");
-		String UserID = vo.getUserID();
+	//스프링 컨트롤러 부분
+	@RequestMapping(value = "/boardPage.do", method = RequestMethod.GET, produces="text/plain;charset=UTF-8")
+	@ResponseBody
+	public String json(Locale locale, Model model) {    
 		
-			
-		ArrayList<MiniHpIntroVO> MiniVO = neighborService.getAllNeighbor(UserID);
-			
-		model.addAttribute("MiniVO", MiniVO);
-		return "Login/boardPage";
+		/* 모든 정보 가져오기 */
+		ArrayList<MiniHpIntroVO> MiniVO = neighborService.getAllNeighbor();
 		
+		
+	    /* 자바오브젝트 -> json, json -> 자바오브젝트 */
+	    Gson gson = new Gson();
+	    
+	    return gson.toJson(MiniVO);
 	}
 
 //  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ 관리자 관련 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
