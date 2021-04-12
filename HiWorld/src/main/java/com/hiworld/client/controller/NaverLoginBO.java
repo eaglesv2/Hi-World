20c4fcd13aab53f8c54d19d957055450f1754543
stux12 <stux12@naver.com>
2021-04-12 22:31:57 +0900

    마지막이란없다

--- HiWorld/src/main/java/com/hiworld/client/controller/NaverLoginBO.java
+++ HiWorld/src/main/java/com/hiworld/client/controller/NaverLoginBO.java
@@ -18,7 +18,7 @@ public class NaverLoginBO {
 	/* client secret : 네이버 애플리케이션 등록 후 발급받은 secret 값 */
 	private final static String CLIENT_SECRET = "zldNDsMNlE";
 	/* redirect_uri: 네이버 로그인 인증의 결과를 전달받을 콜백 URL(URL 인코딩). 애플리케이션을 등록할 때 Callback URL에 설정한 정보입니다. */
-	private final static String REDIRECT_URI = "http://localhost:8081/root/callback";
+	private final static String REDIRECT_URI = "http://ec2-3-36-174-35.ap-northeast-2.compute.amazonaws.com:8080/callback";
 	/* state: 애플리케이션이 생성한 상태 토큰 */
 	private final static String SESSION_STATE = "oauth_state";
 	/* 프로필 조회 API URL */
